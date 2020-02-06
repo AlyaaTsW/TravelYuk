2e16fa50d79b68144a765bfc5d3c22c99713325b
AlyaaTsW <alyaatsw06@gmail.com>
2020-02-05 19:16:13 -0800

    Initial commit

---
+++ app/src/main/java/com/alyaa/travelyuk/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alyaa.travelyuk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
